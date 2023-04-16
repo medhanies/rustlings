5ced56aa1dcda4a9d161107625274ef7950dd9b6
medhanies <medhaniesolomon@yahoo.com>
2023-04-15 19:20:37 -0500

    iter3

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -13,6 +13,11 @@ pub fn factorial(num: u64) -> u64 {
     // For an extra challenge, don't use:
     // - recursion
     // Execute `rustlings hint iterators4` for hints.
+    if num == 0 {
+        0
+    } else {
+        num.iter().rfold(1, |acc, &x| acc * x)
+    }
 }
 
 #[cfg(test)]
